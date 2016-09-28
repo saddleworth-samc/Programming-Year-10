7fe11687a16316314bc6d7d2822ff803c594d04a
Saddleworth-Connor <connorpatrick36@hotmail.com>
2016-09-28 14:53:39 +0100

    Add files via upload

---
+++ My Own Coding.py
@@ -0,0 +1,317 @@
+print ("You have stumbled into the woods.")
+print ("\n")
+import time
+time.sleep (2)#Waits 2 seconds
+print ("You have 2 ways to take...")
+print ("\n") #Print blank line
+import time
+time.sleep (2)#Waits 2 seconds
+print("Which way do you take? ")
+print ("\n") #Print blank line
+import time
+time.sleep (1)#Waits 1 seconds
+rightleft = input("Left or right? ")
+import time
+time.sleep (1)#Waits 3 seconds
+if rightleft == "Left":
+    print ("\n") #Print blank line
+    print("You have turned left, you then find your self deaper in the woods")
+elif rightleft == "right":
+    print ("\n") #Print blank line
+    print("You have gone right and you find a trail of bread crums...")
+print ("\n") #Print blank line
+import time
+time.sleep (3)#Waits 3 seconds
+if rightleft == "Left":
+    print ("\n") #Print blank line
+    print("As you carry on trying to find you way out you hear something behind you.")
+elif rightleft == "right":
+    print ("\n") #Print blank line
+    print("Your hungry and have no food so you start to eat the bread as you follow the trail.")
+print ("\n") #Print blank line
+import time
+time.sleep (3)#Waits 3 seconds
+if rightleft == "Left":
+    print ("\n") #Print blank line
+    print("You hear the noise getting closer")
+elif rightleft == "right":
+    print ("\n") #Print blank line
+    print("As you carry on following and eating the bread crums you bump into an old lady.")
+print ("\n") #Print blank line
+import time
+time.sleep (3)#Waits 3 seconds
+if rightleft == "Left":
+    print ("\n") #Print blank line
+    print("The noise is really close now")
+elif rightleft == "right":
+    print ("\n") #Print blank line
+    print("The old lady wants to play a game..")
+print ("\n") #Print blank line
+import time
+time.sleep (3)#Waits 3 seconds
+if rightleft == "Left":
+    print
+elif rightleft == "right":
+    print("The game is where you have to try get a heads when you flip the coin")
+print ("\n") #Print blank line
+import time
+time.sleep (1)#Waits 3 seconds
+print("You flip the coin and crossing your fingers for good luck.")
+#This code tosses the coin
+import time
+time.sleep (1)#Waits 3 seconds
+import random
+
+def cointoss():
+
+    options = ("heads", "tails")
+    result = random.choice(options)
+    print ("\n") #Print blank line
+    print (result)
+cointoss() #Tosses the coin (Heads or Tails)
+import time
+time.sleep (3)#Waits 3 seconds
+if rightleft == "Left":
+    print
+elif rightleft == "right":
+    print ("\n") #Print blank line
+    print("After you flip the coin she trys to grab you, you have to get out of there, you start to run for 5 minuets until you can't see her anymore, but you hear her screem")
+print ("\n") #Print blank line
+import time
+time.sleep (3)#Waits 3 seconds
+print("You think of running or hiding...")
+print ("\n") #Print blank line
+import time
+time.sleep (3)#Waits 3 seconds
+print("Which one do you do?")
+import time
+time.sleep (1)#Waits 1 seconds
+print ("\n") #Print blank line
+runhide = input("Hide or run? ")
+import time
+time.sleep (3)#Waits 3 seconds
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("You hide in a bush, you see it run past.")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("You run through and out of trees and bushes until you know your far enough away.")
+print ("\n") #Print blank line
+import time
+time.sleep (2)#Waits 1 seconds
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("After hiding for a few minutes, you get out of the bush and start walking watching every corner...")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("After running away, you fall down a whole which is atleast 7 metres tall and 3 metres long")
+print ("\n") #Print blank line
+import time
+time.sleep (2)#Waits 1 seconds
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("You carry on walking until you find a house")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("You struggle to get out until someone or something drops a roap down into the whole...")
+print ("\n") #Print blank line
+import time
+time.sleep (4)#Waits 4 seconds
+print ("\n") #Print blank line
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("as you approach the house, the door creeks open...")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("Once you climbed up the roap, there is a black figure standing in front of you...")
+print ("\n") #Print blank line
+import time
+time.sleep (4)#Waits 4 seconds
+print ("\n") #Print blank line
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("You get closer and closer to the house and that's when you notice it's a house made out of ginger-bread and a voice says'")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("After only a few seconds the figure speaks and says")
+import time
+time.sleep (2)#Waits 2 seconds
+print ("\n") #Print blank line
+name = input("What's your name?'")
+print ("Hello "+ name)
+print("\n")
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("After this mysterious voice asks your name it welcoms you in.")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("After the figure greets you, it leeds you to...")
+import time
+time.sleep (4)#Waits 4 seconds
+print ("\n") #Print blank line
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("As you enter the strange and mystical house which is made out of ginger-bread you notice that it's made of more than just ginger-bread...'")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("The figure has lead you to a cottage where you notice it's made of ginger-bread.'")
+import time
+time.sleep (4)#Waits 4 seconds
+print ("\n") #Print blank line
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("It's made out of bones! bones that look like human ones, human skulls and ribs every where...")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("After following this figure into the ginger-bread house you notice it's also made out of sweats and other candy's.")
+import time
+time.sleep (4)#Waits 4 seconds
+print ("\n") #Print blank line
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("As you snap out of fear, you dart it to the door, but suddenly it slams shut")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("from not eating for hours you decide to eat some of the house.")
+import time
+time.sleep (4)#Waits 4 seconds
+print ("\n") #Print blank line
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("You stumble backwards tripping over a bone")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("You keep on eating until you see a shadow fly past the window")
+import time
+time.sleep (4)#Waits 4 seconds
+print ("\n") #Print blank line
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("hitting your head, you black out...")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("You dismiss this and carry on eating stuffing your mouth with every candy and chocolate possible")
+import time
+time.sleep (4)#Waits 4 seconds
+print ("\n") #Print blank line
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("Once you awake you can't move or shout for help'")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("suddenly your hit over the head, you go unconcious")
+import time
+time.sleep (4)#Waits 4 seconds
+print ("\n") #Print blank line
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("when your eyes have adjusted you see what was hunting you all this time, it was a witch one that had a pointy nose and a bug wart on her cheak'")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("You wake up on a metal tray strapped down")
+import time
+time.sleep (4)#Waits 4 seconds
+print ("\n") #Print blank line
+if runhide == "Hide":
+    print ("\n") #Print blank line
+    print("You suddenly see the stove burst into flames'")
+elif runhide == "run":
+    print ("\n") #Print blank line
+    print("it's a witch a scary looking one too, you thought this wasn't real you knew these things weren't real, but they are...'")
+shoutquiet = input("Try shout or keep quiet? ")
+import time
+time.sleep (1)#Waits 1 seconds
+if shoutquiet == "Try shout":
+    print ("\n") #Print blank line
+    print("You shout, the witch knows your awake so she throws you into the stove..." "THE END!")
+    print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄▄▄░░░░░")
+    print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░███▀█░░░░")
+    print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░███░██░░░")
+    print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░███░░██░░░")
+    print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░▄██░░░░██░░")
+    print("░░░░░░░░░░░░░░░░▄▄█▀▀▀▀█▄▄▄▄███▄░░░██░░")
+    print("░░░░░░░░░░░░░▄█▀░░░░░░░░░▀▀▀▀█░▀█░░██░░")
+    print("░░░░░░░░░░░░█▀░░░░░░░░░░░░░░░▀█▄█▀▀▀░░░")
+    print("░░░░░░░░░░░▄▀░░░░░░░░░░░░░░░░▀█▄░░░░░░░")
+    print("░░░░░░░░░░░▀█▄░█░░▄▀░░░░░░░▄▄█░░░░░░░░░")
+    print("░░░░░░░░▄▄▄▄█▀▀▀██▄▄░░░░░░▄▀░░░░░░░░░░░")
+    print("░░░░▄▄██▄▀▀░░░░░█▀░░░░░░▄██▄░░░░░░░░░░░")
+    print("░░▄██▀▀░░░░░░░░▄█░░░░░▄█▀▄█░▀▀█▄▄▄▄▄▄▄▄")
+    print("░██▀░░░░░░░░▄██░░░░░░▄▀▄▄▀░░░░░░░░░░░░░")
+    print("░██▀░░░░░░░░▄██░░░░░░▄▀▄▄▀░░░░░░░░░░░░░")
+    print("█▀▀░░░░░░▄█▀▄░▀▄▄▄▄██▀▀▀░░░░░█░░░░░░░░░")
+    print("█░░░░░▄▄▀░░░░█░░░░░░░░░░░░░░░▀░░░░░░░░░")
+    print("░▀▀▀▀▀▀░░░░░░░█░░░░░░░░░░░░░░░▀▀█▀▀▀▀▀▀")
+    print("░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░░█▀░░░░░░")
+    print("░░░░░░░░░░░░░░█░░░░░░░░░░░░░░░▄█░░░░░░░")
+    print("                FACE PALM              ")
+elif shoutquiet == "keep quiet":
+    print ("\n") #Print blank line
+    print("You keep quiet and she doesn't know your awake'")
+import time
+time.sleep (3)#Waits 3 seconds
+if shoutquiet == "Shout":
+    print ("\n") #Print blank line
+    print
+elif shoutquiet == "keep quiet":
+    print ("\n") #Print blank line
+    print("When she isn't looking you wiggle trying your hardest to get out of the straps'")
+    print ("\n") #Print blank line
+import time
+time.sleep (1)#Waits 1 seconds
+if shoutquiet == "Shout":
+    print ("\n") #Print blank line
+    print
+elif shoutquiet == "keep quiet":
+    print ("\n") #Print blank line
+    print("She makes her way over to you...")
+    print ("\n") #Print blank line
+import time
+time.sleep (1)#Waits 1 seconds
+if shoutquiet == "Shout":
+    print ("\n") #Print blank line
+    print
+elif shoutquiet == "keep quiet":
+    print ("\n") #Print blank line
+    print("Suddenly she stops and growls, she slowly walks over to the door changing her looks on the way making her self look like a human")
+    print ("\n") #Print blank line
+import time
+time.sleep (1)#Waits 1 seconds
+if shoutquiet == "Shout":
+    print ("\n") #Print blank line
+    print
+elif shoutquiet == "keep quiet":
+    print ("\n") #Print blank line
+    print("You manage to get your hands unstrapped and you take the tape off your mouth and when she opens the door you shout")
+shout = input("What would you shout? ")
+print ("Please "+ shout)
+if shoutquiet == "Shout":
+    print ("\n") #Print blank line
+    print
+elif shoutquiet == "keep quiet":
+    print ("\n") #Print blank line
+    print("After shouting the witch tries to run at you but someone stop her...")
+if shoutquiet == "Shout":
+    print ("\n") #Print blank line
+    print
+elif shoutquiet == "keep quiet":
+    print ("\n") #Print blank line
+    print("It's a girl and a boy, the reason why the witch did not reach is because they killed her... TO BE CONTINUED'")
+    print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+    print("░░░░░░░▄▄▀▀▀▀▀▀▀▀▀▀▄▄█▄░░░░▄░░░░█░░░░░░░")
+    print("░░░░░░█▀░░░░░░░░░░░░░▀▀█▄░░░▀░░░░░░░░░▄░")
+    print("░░░░▄▀░░░░░░░░░░░░░░░░░▀██░░░▄▀▀▀▄▄░░▀░░")
+    print("░░▄█▀▄█▀▀▀▀▄░░░░░░▄▀▀█▄░▀█▄░░█▄░░░▀█░░░░")
+    print("░▄█░▄▀░░▄▄▄░█░░░▄▀▄█▄░▀█░░█▄░░▀█░░░░█░░░")
+    print("▄█░░█░░░▀▀▀░█░░▄█░▀▀▀░░█░░░█▄░░█░░░░█░░░")
+    print("██░░░▀▄░░░▄█▀░░░▀▄▄▄▄▄█▀░░░▀█░░█▄░░░█░░░")
+    print("██░░░░░▀▀▀░░░░░░░░░░░░░░░░░░█░▄█░░░░█░░░")
+    print("██░░░░░░░░░░░░░░░░░░░░░█░░░░██▀░░░░█▄░░░")
+    print("██░░░░░░░░░░░░░░░░░░░░░█░░░░█░░░░░░░▀▀█▄")
+    print("██░░░░░░░░░░░░░░░░░░░░█░░░░░█░░░░░░░▄▄██")
+    print("░██░░░░░░░░░░░░░░░░░░▄▀░░░░░█░░░░░░░▀▀█▄")
+    print("░▀█░░░░░░█░░░░░░░░░▄█▀░░░░░░█░░░░░░░▄▄██")
+    print("░▄██▄░░░░░▀▀▀▄▄▄▄▀▀░░░░░░░░░█░░░░░░░▀▀█▄")
+    print("░░▀▀▀▀░░░░░░░░░░░░░░░░░░░░░░█▄▄▄▄▄▄▄▄▄██")
+    print("░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░")
+    print("             THUMBS UP                  ")
